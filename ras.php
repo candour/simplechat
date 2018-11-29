6dbf9d9a43371566235c48ef70b253c2a710c929
candour <richard@semmens.eu>
2018-11-29 21:38:08 +0000

    First commit

---
+++ ras.php
@@ -0,0 +1,13 @@
+<form action="ras-upload.php" method="post" enctype="multipart/form-data">
+  Please choose a file: <input type="file" name="uploadFile"><br>
+  <input type="submit" value="Upload File">
+</form>
+
+<br />
+<?php
+$upload_dir = '/var/www/semmens.info/web/uploads';
+if (is_dir($upload_dir) && is_writable($upload_dir)) {
+  echo "Can upload";
+} else {
+  echo "Can't upload";
+}
